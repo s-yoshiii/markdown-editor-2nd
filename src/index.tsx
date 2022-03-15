e8f3c617c27c2f89dac802f71834021b26cee6e2
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-03-16 00:49:10 +0900

    chakraUIの導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,26 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-const Main = <h1>MarkDown Editor</h1>;
+import { Box, ChakraProvider, Flex, Text } from '@chakra-ui/react';
+const Main = (
+  <ChakraProvider>
+    <header>
+      <h1>MarkDown Editor</h1>
+    </header>
+    <Flex color='white'>
+      <Box w='25%' bg='#F6F2D4'>
+        <Text>Box</Text>
+      </Box>
+      <Box w='25%' bg='#95D1CC'>
+        <Text>Box</Text>
+      </Box>
+      <Box w='25%' bg='#5584AC'>
+        <Text>Box</Text>
+      </Box>
+      <Box w='25%' bg='#22577E'>
+        <Text>Box</Text>
+      </Box>
+    </Flex>
+  </ChakraProvider>
+);
 
 render(Main, document.getElementById('app'));
