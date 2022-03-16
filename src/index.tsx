ee296864c5a81aaef791b6d638ec1d5b78743fdc
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-03-17 00:49:43 +0900

    chakraUIでスタイルを整える

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,50 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Box, ChakraProvider, Flex, Text } from '@chakra-ui/react';
+import {
+  Box,
+  ChakraProvider,
+  extendTheme,
+  Flex,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
+import { Editor } from './pages/editor';
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#F6F2D4',
+      200: '#95D1CC',
+      300: '#5584AC',
+      400: '#22577E',
+    },
+  },
+});
 const Main = (
-  <ChakraProvider>
-    <header>
-      <h1>MarkDown Editor</h1>
-    </header>
+  <ChakraProvider theme={theme} resetCSS>
+    <Heading
+      as='h1'
+      size='lg'
+      p={[4, 2]}
+      position={'fixed'}
+      top={0}
+      left={0}
+      w='100%'
+      h={12}
+    >
+      MarkDown Editor
+    </Heading>
+    <Editor />
     <Flex color='white'>
-      <Box w='25%' bg='#F6F2D4'>
+      <Box w='25%' bg='brand.100'>
         <Text>Box</Text>
       </Box>
-      <Box w='25%' bg='#95D1CC'>
+      <Box w='25%' bg='brand.200'>
         <Text>Box</Text>
       </Box>
-      <Box w='25%' bg='#5584AC'>
+      <Box w='25%' bg='brand.300'>
         <Text>Box</Text>
       </Box>
-      <Box w='25%' bg='#22577E'>
+      <Box w='25%' bg='brand.400'>
         <Text>Box</Text>
       </Box>
     </Flex>
---
+++ src/pages/editor.tsx
@@ -0,0 +1,9 @@
+import { Text } from '@chakra-ui/react';
+import * as React from 'react';
+export const Editor: React.FC = () => {
+  return (
+    <>
+      <Text>Editor</Text>
+    </>
+  );
+};
