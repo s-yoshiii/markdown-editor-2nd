dc87cffcfe61000b25cc745bad50579fa6ce3537
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-03-19 00:41:46 +0900

    Textareaのスタイル調整

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Textarea } from '@chakra-ui/react';
 import * as React from 'react';
 import { Header } from '../components/header';
 export const Editor: React.FC = () => {
@@ -7,9 +7,18 @@ export const Editor: React.FC = () => {
       <Header />
       <Flex position='fixed' paddingTop='14' w='100vw' h='100vh'>
         <Box w='50vw' borderRight='1px'>
+          <Textarea
+            placeholder='Here is a sample placeholder'
+            isFullWidth
+            height='100%'
+            variant='flushed'
+            p='3'
+            isInvalid
+          />
+        </Box>
+        <Box w='50vw' p='3'>
           Text入力エリア
         </Box>
-        <Box w='50vw'>Text入力エリア</Box>
       </Flex>
     </>
   );
