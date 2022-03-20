a55fe300a5f5941bf582f8743249a2145a12cb90
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-03-21 00:23:00 +0900

    WebStorageを使用して入力内容をブラウザに保存

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,12 +1,10 @@
 import { Box, Flex, Textarea } from '@chakra-ui/react';
 import * as React from 'react';
-const { useState } = React;
+import { useStateWithStorage } from '../hooks/use_state_with_storage';
 import { Header } from '../components/header';
 const StorageKey = 'pages/editor:textarea';
 export const Editor: React.FC = () => {
-  const [text, setText] = useState<string>(
-    localStorage.getItem(StorageKey) || ''
-  );
+  const [text, setText] = useStateWithStorage('', StorageKey);
   return (
     <>
       <Header />
@@ -18,7 +16,6 @@ export const Editor: React.FC = () => {
             height='100%'
             variant='flushed'
             p='3'
-            isInvalid
             onChange={(e) => {
               setText(e.target.value);
             }}
