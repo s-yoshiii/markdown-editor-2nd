1ec19fefba61255e96f8d121260455c0f66bae5c
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-04-06 23:27:12 +0900

    webpackプラグイン追加、publicPath修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.tsx',
   module: {
@@ -13,6 +16,9 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: 'html-loader',
+        options: {
+          minimize: false,
+        },
       },
     ],
   },
@@ -22,7 +28,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    publicPath: 'auto',
+    publicPath: '/',
   },
   devServer: {
     static: {
@@ -32,9 +38,25 @@ module.exports = {
     open: true,
     watchFiles: ['src/**/*'],
   },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
   plugins: [
-    new WorkboxWebpackPlugin.GenerateSW({
-      swDest: __dirname + '/dist/sw.js',
+    new CleanWebpackPlugin(),
+    new WebpackPwaManifest({
+      name: 'MarkdownEditor',
+      filename: 'manifest.json',
+      short_name: 'MarkdownEditor',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ffffff',
+      start_url: '/#/editor',
+    }),
+    new GenerateSW({
+      swDest: path.resolve(__dirname, 'dist/sw.js'),
       maximumFileSizeToCacheInBytes: 5000000000,
       clientsClaim: true,
       skipWaiting: true,
@@ -42,6 +64,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
       inject: 'body',
+      minify: false,
     }),
   ],
 };
